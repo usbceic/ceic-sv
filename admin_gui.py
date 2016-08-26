6f4b2d98ac5cfba40bf98fa874e20f687041dacf
cserradag96 <cserradag96@gmail.com>
2016-08-26 17:32:47 -0400

    Nuevo

--- admin_gui.py
+++ admin_gui.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
 ####################################################################################################################################
@@ -24,7 +24,8 @@
 import gui_rc                   # Módulo que contiene los recursos de la interfaz
 from PyQt4.uic import *         # Módulo con las herramientas parar tranajar los archivos .ui
 from PyQt4.QtCore import *      # Módulo con procedimientos de Qt
-from PyQt4.QtGui import *       # Modulo con estructuras de Qt
+from PyQt4.QtGui import *       # Módulo con estructuras de Qt
+from DBManager import *         # Módulo manejador de la base de datos
 
 ####################################################################################################################################
 ## CONSTANTES:
@@ -42,6 +43,9 @@ styles = ["black.css", "default.css", "fuchsia.css", "green.css", "orange.css", 
 
 # Interfaz .ui creada con qt designer
 form_class = loadUiType(UIpath+MainUI)[0]
+
+# Constante de primer inicio
+A = True
 
 ####################################################################################################################################
 ## PROCEDIMIENTOS:
@@ -66,24 +70,85 @@ clientList = ["513264", "28436485", "32564336", "105746567", "280765423", "67012
 ####################################################################################################################################
 
 class sistema_window(QMainWindow, form_class, QWidget):
+    #-------------------------------------------------------------------------------------------------------------------------------
+    # Constructor de la clase
+    #-------------------------------------------------------------------------------------------------------------------------------
+
     def __init__(self, parent=None):
-        # Se inicia y configura la interfaz
+        #---------------------------------------------------------------------------------------------------------------------------
+        # Iniciar y configurar la interfaz y la base de datos
+        #---------------------------------------------------------------------------------------------------------------------------
+
+        # Interfaz
         QMainWindow.__init__(self, parent)
         self.setupUi(self)
 
+        # Manejador de la base de datos
+        self.DB = DBManager("carlos", "curtis", True)
+
+        #---------------------------------------------------------------------------------------------------------------------------
+        # Listas necesarias para algunos métodos útiles
+        #---------------------------------------------------------------------------------------------------------------------------
+
+        # Parametros para buscar en la tabla de productos
+        self.productsParams0 = ["name"]
+        self.productsParams1 = ["name", "cost", "category"]
+
+        # Listas de nombres de los productos
+        self.productsNames = self.getProductList(self.productsParams0, 0)                     # Completa
+        self.productsNamesAvailable = self.getProductList(self.productsParams0, 1)            # Disponibles
+        self.productsNamesNotAvailable = self.getProductList(self.productsParams0, 2)         # No disponibles
+
+        # Listas con información para las tablas de productos
+        self.productsInfo = self.getProductList(self.productsParams1, 0)                     # Completa
+        self.productsInfoAvailable = self.getProductList(self.productsParams1, 1)            # Disponibles
+        self.productsInfoNotAvailable = self.getProductList(self.productsParams1, 2)         # No disponibles
+
+        # Barras de búsqueda por cédula:
+        self.ciSearch = [self.lineE17, self.lineE47, self.lineE52, self.lineE57]
+
+        # Barras de búsqueda por nombre de producto:
+        self.productSearch = [self.lineE23, self.lineE26, self.lineE33]
+
+        # Apartado de productos en inventario
+        self.productsRO0 = [self.lineE26, self.lineE27, self.lineE28, self.lineE29, self.lineE30, self.lineE31, self.lineE32]
+        self.productsRO1 = [self.lineE27, self.lineE28, self.lineE29, self.lineE30, self.lineE31, self.lineE32]
+        self.productsRO2 = [self.lineE27, self.lineE28, self.lineE29, self.lineE31, self.lineE32]
+        self.productsRO3 = [self.lineE30]
+
+        # Apartado de lotes en inventario
+        self.lotsRO0 = [self.lineE34, self.lineE35, self.lineE36, self.lineE37, self.lineE38, self.lineE39]
+        self.lotsRO1 = [self.lineE34, self.lineE35, self.lineE36, self.lineE37, self.lineE38, self.lineE39]
+        self.lotsRO2 = [self.lineE34, self.lineE35, self.lineE36, self.lineE38, self.lineE39]
+        self.lotsRO3 = [self.lineE37]
+
+        # Tablas
+        self.tables = [self.table0, self.table1, self.table2, self.table3, self.table4, self.table5, self.table6, self.table7,
+                        self.table8, self.table9, self.table10, self.table11]
+
+        #---------------------------------------------------------------------------------------------------------------------------
+        # Cargar configuraciones iniciales
+        #---------------------------------------------------------------------------------------------------------------------------
+
         #Configuración de la barra de búsqueda de cliente por CI
-        self.LEmodel = QStringListModel()
-        self.LEmodel.setStringList(clientList)
-        self.ciCompleter = QCompleter()
-        self.ciCompleter.setModel(self.LEmodel)
-        self.lineE17.setValidator(QIntValidator(0, 100000000))
-        self.lineE17.setCompleter(self.ciCompleter)
+        self.setupSearchBar(self.ciSearch, clientList, True)
+
+        # Configuración de las barras de búsqueda por nombre de producto
+        self.setupSearchBar(self.productSearch, self.productsNames)
 
         # Se connectan los botones entre otras cosas con algunos de los métodos definidos a continuación
         QMetaObject.connectSlotsByName(self)
 
         # Variable de control para los QPushButton
         self.clicked = False
+
+        # Configurar tablas
+        self.setupTables()
+
+        # Setup de las distintas tablas
+        self.updateTable(self.table0, self.productsInfoAvailable)
+        self.updateTable(self.table1, self.productsInfoNotAvailable)
+        self.updateTable(self.table2, self.productsInfo)
 
     #-------------------------------------------------------------------------------------------------------------------------------
     # Métodos de la clase
@@ -109,6 +174,61 @@ class sistema_window(QMainWindow, form_class, QWidget):
     # Cambiar el tema de la interfáz
     def setStyle(self, name):
         if self.click(): self.setStyleSheet(getStyle(name))
+
+    # Método para buscar en un LineEdit
+    def setupSearchBar(self, listLE, itemsList, numValidator = False):
+        LEmodel = QStringListModel()
+        LEmodel.setStringList(itemsList)
+        LEcompleter = QCompleter()
+        LEcompleter.setModel(LEmodel)
+        for lineE in listLE:
+            if numValidator: lineE.setValidator(QIntValidator(0, 100000000))
+            lineE.setCompleter(LEcompleter)
+
+    # Obtener una lista con los nombres de todos los productos
+    def getProductList(self, params, mode):
+        productsList = self.DB.getItemInfo(productsTable, params, mode)
+        if len(params) == 1:
+            productsNames = []
+            for i in range(len(productsList)):
+                productsNames += productsList[i]
+            return productsNames
+
+        else: return productsList
+
+    # Borrar contenido de uno o más LineEdit:
+    def clearLE(self, listLE):
+        for lineE in listLE: lineE.setText("")
+
+    # Marcar en solo lectura uno o más LineEdit:
+    def ReadOnlyLE(self, listLE, boolean):
+        for lineE in listLE: lineE.setReadOnly(boolean)
+
+    # Cambio de estado de los line edits en un apartado
+    def changeRO(self, listaLE0, boolean = True, listaLE1 = []):
+        if self.click():
+            self.ReadOnlyLE(listaLE0, boolean)
+            if len(listaLE1) != 0: self.ReadOnlyLE(listaLE1, not(boolean))
+
+    def setupTables(self):
+        for table in self.tables:
+            table.horizontalHeader().setResizeMode(0, QHeaderView.Stretch)
+
+    # Método para refrescar la tabla de canciones en la ventana
+    def updateTable(self, table, itemsList):
+        #for i in reversed(range(table.rowCount())): table.removeRow(i)
+
+        #table.setRowCount(len(itemsList))
+        for i in range(len(itemsList)):
+            name = QTableWidgetItem(str(itemsList[i][0]))
+            cost = QTableWidgetItem(str(itemsList[i][1]))
+            category = QTableWidgetItem(str(itemsList[i][2]))
+            table.setItem(i, 0, name)
+            table.setItem(i, 1, cost)
+            table.setItem(i, 2, category)
+
+        self.elem_actual = 0
+        if len(itemsList) > 0: table.selectRow(self.elem_actual)
 
     #-------------------------------------------------------------------------------------------------------------------------------
     # Configuración de los botones para cambio de página de los stacked:
@@ -219,10 +339,41 @@ class sistema_window(QMainWindow, form_class, QWidget):
     def on_yellowGUI_pressed(self): self.setStyle(styles[7])
 
     #-------------------------------------------------------------------------------------------------------------------------------
+    # Configuración de botones del inventario
+    #-------------------------------------------------------------------------------------------------------------------------------
 
+    # Radio button para agregar nuevos productos
+    def on_rbutton5_pressed(self): self.changeRO(self.productsRO1, False)
 
+    # Radio button para consultar productos
+    def on_rbutton6_pressed(self): self.changeRO(self.productsRO1)
 
+    # Radio button para editar productos
+    def on_rbutton7_pressed(self): self.changeRO(self.productsRO2, False, self.productsRO3)
 
+    # Radio button para eliminar productos
+    def on_rbutton8_pressed(self): self.changeRO(self.productsRO1)
 
+    # Boton "Aceptar" en el apartado de productos
+    def on_pbutton11_pressed(self):
+        if self.click():
+            if self.rbutton5.isChecked():
+                name = self.lineE26.text()
+                cost = self.lineE27.text()
+                category = self.lineE28.text()
+                self.DB.createProduct(name, cost)
+                self.setupProductsSearchs()
+                self.clearLE(self.productsRO0)
 
+    # Boton "Aceptar" en el apartado de lotes
+    def on_pbutton13_pressed(self):
+        if self.click():
+            if self.rbutton9.isChecked():
+                productID = self.lineE26.text()
+                cost = self.lineE27.text()
+                self.DB.createLot(productID, cost)
+                self.clearLE(self.productsRO0)
 
+####################################################################################################################################
+## FIN :)
+####################################################################################################################################
