f862efe9e6993e57cd424b6676f9bff9db1a05c1
Carlos Serrada <cserradag96@gmail.com>
2017-04-04 17:09:11 -0400

    Limpieza

--- PSQL/remove_functions.sql
+++
@@ -1,5 +0,0 @@
--- Base de Datos del Sistema de Ventas del CEIC
--- Hecho por Christian Oliveros
-
--- Quitar funciones Generales si existen
-DROP FUNCTION IF EXISTS check_password(TEXT, TEXT);
--- PSQL/remove_types.sql
+++
@@ -1,5 +0,0 @@
--- Base de Datos del Sistema de Ventas del CEIC
--- Hecho por Christian Oliveros
-
--- Quitar types y Enums si existen
-DROP TYPE IF EXISTS check_password_type;
