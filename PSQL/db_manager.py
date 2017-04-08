7d0d4431939e5334b61f656d05d1df3a755b3c48
Carlos Serrada <cserradag96@gmail.com>
2017-04-08 19:05:08 -0400

    Añadidos métodos para usuarios de la db

--- PSQL/db_manager.py
+++ PSQL/db_manager.py
@@ -21,6 +21,7 @@
 from models import *
 from session import startSession
 from sqlalchemy import func, distinct
+from passlib.hash import bcrypt
 
 ###################################################################################################################################################################################
 ## DECLARACIÓN DEL MANEJADOR:
@@ -45,6 +46,52 @@ class DBManager(object):
         for i in query: print(i)
         """
 
+    #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # MÉTODOS PARA EL CONTROL DE USUARIOS:
+    #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    # Método para verificar que un usuario existe.
+    # Retorna true cuando el usuario existe y false en caso contario
+    def userExist(self, username):
+        count = self.session.query(User).filter_by(username=username).count()
+        if count == 0:
+            print("El usuario " + username + " NO existe")
+            return False
+        else:
+            print("El usuario " + username + " existe")
+            return True
+
+    # Método para crear un usuario nuevo
+    # Retorna true cuando el usuario es creado satisfactoreamente y false cuando no se puede crear o cuando ya existia el usuario
+    def createUser(self, username, password, firstname, lastname, email, permission_mask):
+        if not self.userExist(username):
+            newUser = User(username=username, password=bcrypt.hash(password), firstname=firstname, lastname=lastname, email=email, permission_mask=permission_mask)
+            self.session.add(newUser)
+            try:
+                self.session.commit()
+                print("Se ha creado correctamente el usuario " + username)
+                return True
+            except Exception as e:
+                print("Error desconocido al crear el usuario " + username +":", e)
+                m.session.rollback()
+                return False
+        else:
+            print("No se puede crear el usuario " + username + " porque ya existe")
+            return False
+
+    # Método para verificar correspondencia entre usuario y contraseña
+    # Retorna true cuando los datos coinciden con los almacenados en la base de datos y retorna false cuando no coinciden o cuando el usuario no existe
+    def checkPassword(self, username, password):
+        if self.userExist(username):
+            hashedPass = self.session.query(User.password).filter_by(username=username).one()[0]
+            if bcrypt.verify(password, hashedPass):
+                print("Información de inicio de sesión válidada correctamente")
+                return True
+            else:
+                print("La contraseña no corresponde al usuario especificado")
+                return False
+        return False
+
 ###################################################################################################################################################################################
 ## PRUEBAS:
 ###################################################################################################################################################################################
@@ -53,13 +100,13 @@ class DBManager(object):
 if __name__ == '__main__':
     m = DBManager("sistema_ventas", "hola", True)
     l = Valid_language(lang_name="ES")
-    m.session.add(l)
+    """m.session.add(l)
     try:
         m.session.commit()
     except Exception as e:
         print("Excepcion de Lenguaje:", e)
         m.session.rollback()
-    
+
     try:
         b = m.session.query(Book).filter_by(title="Prueba", lang=l.lang_name).one()
     except Exception as e:
@@ -67,13 +114,12 @@ if __name__ == '__main__':
         import datetime
         b = Book(title="Prueba", book_year=datetime.datetime.now(), lang=l.lang_name)
         m.session.add(b)
-        m.session.commit()
+        m.session.commit()"""
 
+    m.createUser("tobi", "loveurin", "obito", "uchiha", "tobi@akatsuki.com", 3)
+    m.checkPassword("tobi", "loveurin")
 
-
-    
-
-    s = Subject(subject_code="CI123", subject_name="Test")
+    """s = Subject(subject_code="CI123", subject_name="Test")
     m.session.add(s)
     try:
         m.session.commit()
@@ -90,5 +136,5 @@ if __name__ == '__main__':
         print(book)
 
     for subjects in b.subjects:
-        print(subjects)
+        print(subjects)"""
 
