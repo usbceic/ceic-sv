32edaba0f74dfc3cf3c7d8b9fb0ac1ecdb9186ea
Carlos Serrada <cserradag96@gmail.com>
2017-04-08 20:01:27 -0400

    Añadido método para cambio de contraseña

--- PSQL/db_manager.py
+++ PSQL/db_manager.py
@@ -20,7 +20,7 @@
 
 from models import *
 from session import startSession
-from sqlalchemy import func, distinct
+from sqlalchemy import func, distinct, update
 from passlib.hash import bcrypt
 
 ###################################################################################################################################################################################
@@ -33,25 +33,17 @@ class DBManager(object):
         super(DBManager, self).__init__()
         self.session = startSession(name, password, debug)
 
-        """
-        Insert Example
-        user1 = User(username="saitama", password="xd", firstname="carlos", lastname="serrada", email="xd@gmail.com", permission_mask=2)
-        self.session.add(user1)
-        self.session.commit()
-        """
-
-        """
-        Query Example
-        query = self.session.query(func.count(distinct(User.username)))
-        for i in query: print(i)
-        """
-
     #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     # MÉTODOS PARA EL CONTROL DE USUARIOS:
     #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    # Método para verificar que un usuario existe.
-    # Retorna true cuando el usuario existe y false en caso contario
+    """
+    Método para verificar que un usuario existe.
+     - Retorna True:
+        * Cuando el usuario existe
+     - Retorna False:
+        * Cuando el usuario NO existe
+    """
     def userExist(self, username):
         count = self.session.query(User).filter_by(username=username).count()
         if count == 0:
@@ -61,8 +53,14 @@ class DBManager(object):
             print("El usuario " + username + " existe")
             return True
 
-    # Método para crear un usuario nuevo
-    # Retorna true cuando el usuario es creado satisfactoreamente y false cuando no se puede crear o cuando ya existia el usuario
+    """
+    Método para crear un usuario nuevo
+     - Retorna True:
+        * Cuando el usuario es creado satisfactoreamente
+     - Retorna False:
+        * Cuando ya existe el usuario
+        * Cuando no se puede crear
+    """
     def createUser(self, username, password, firstname, lastname, email, permission_mask):
         if not self.userExist(username):
             newUser = User(username=username, password=bcrypt.hash(password), firstname=firstname, lastname=lastname, email=email, permission_mask=permission_mask)
@@ -79,8 +77,14 @@ class DBManager(object):
             print("No se puede crear el usuario " + username + " porque ya existe")
             return False
 
-    # Método para verificar correspondencia entre usuario y contraseña
-    # Retorna true cuando los datos coinciden con los almacenados en la base de datos y retorna false cuando no coinciden o cuando el usuario no existe
+    """
+    Método para verificar correspondencia entre usuario y contraseña
+     - Retorna True:
+        * Cuando los datos coinciden con los almacenados en la base de datos
+     - Retorna False:
+        * Cuando el usuario no existe
+        * Cuando los datos NO coinciden con los almacenados en la base de datos
+    """
     def checkPassword(self, username, password):
         if self.userExist(username):
             hashedPass = self.session.query(User.password).filter_by(username=username).one()[0]
@@ -92,6 +96,27 @@ class DBManager(object):
                 return False
         return False
 
+    """
+    Método para cambiar la contraseña de un usuario
+     - Retorna True:
+        * Cuando logra cambiar la contraseña correctamente
+     - Retorna False:
+        * Cuando el usuario no existe
+        * Cuando la contraseña antigua no coincide con la almacenada en la base de datos
+        * Cuando no pudo cambiarse la contraseña por alguna otra razón
+    """
+    def changePassword(self, username, oldPass, newPass):
+        if self.checkPassword(username, oldPass):
+            try:
+                self.session.execute(update(User).where(User.username==username).values(password=bcrypt.hash(newPass)))
+                self.session.commit()
+                print("Se ha cambiado la contraseña del ususario " + "satisfactoriamente")
+                return True
+            except Exception as e:
+                print("Ha ocurrido un error al intentar cambiar la contraseña del usuario " + username, e)
+                return False
+        return False
+
 ###################################################################################################################################################################################
 ## PRUEBAS:
 ###################################################################################################################################################################################
@@ -99,8 +124,23 @@ class DBManager(object):
 # Prueba
 if __name__ == '__main__':
     m = DBManager("sistema_ventas", "hola", True)
+
+    """
+    Insert Example
+    user1 = User(username="saitama", password="xd", firstname="carlos", lastname="serrada", email="xd@gmail.com", permission_mask=2)
+    m.session.add(user1)
+    m.session.commit()
+    """
+
+    """
+    Query Example
+    query = m.session.query(func.count(distinct(User.username)))
+    for i in query: print(i)
+    """
+
+    """
     l = Valid_language(lang_name="ES")
-    """m.session.add(l)
+    m.session.add(l)
     try:
         m.session.commit()
     except Exception as e:
@@ -114,12 +154,9 @@ if __name__ == '__main__':
         import datetime
         b = Book(title="Prueba", book_year=datetime.datetime.now(), lang=l.lang_name)
         m.session.add(b)
-        m.session.commit()"""
+        m.session.commit()
 
-    m.createUser("tobi", "loveurin", "obito", "uchiha", "tobi@akatsuki.com", 3)
-    m.checkPassword("tobi", "loveurin")
-
-    """s = Subject(subject_code="CI123", subject_name="Test")
+    s = Subject(subject_code="CI123", subject_name="Test")
     m.session.add(s)
     try:
         m.session.commit()
@@ -136,5 +173,23 @@ if __name__ == '__main__':
         print(book)
 
     for subjects in b.subjects:
-        print(subjects)"""
+        print(subjects)
+    """
 
+    """Pruebas de los métodos para usuarios"""
+
+    # Probar createUser
+    print("\nPrueba del método createUser\n")
+    m.createUser("tobi", "loveurin", "obito", "uchiha", "tobi@akatsuki.com", 3)
+
+    # Probar checkPassword
+    print("\nPrueba del método checkPassword\n")
+    m.checkPassword("tobi", "loveurin")
+
+    # Probar changePassword
+    print("\nPrueba del método changePassword\n")
+    m.changePassword("tobi", "loveurin", "hateukakashi")
+    print("")
+    m.checkPassword("tobi", "loveurin")
+    print("")
+    m.checkPassword("tobi", "hateukakashi")
