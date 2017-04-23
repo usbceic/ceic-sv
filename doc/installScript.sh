a532da1c3fa8f310210b5fe634979abc896db271
Pablo <pablodbc30@gmail.com>
2017-04-23 19:35:59 -0400

    Tratando de quitar el frontend

--- doc/installScript.sh
+++ doc/installScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Bienvenido al instalador de CEIC Suite (Versión netinstall)"
 echo "Instalando dependencias..."
-sudo env DEBIAN_FRONTEND=noninteractive apt-get install python3-pip python3-pyqt4 postgresql-9.5 -y
+sudo env DEBIAN_FRONTEND=noninteractive apt-get install python3-pip python3-pyqt4 postgresql-9.5 -yq
 sudo env DEBIAN_FRONTEND=noninteractive python3 -m pip install -r requirements
 echo "¿Tiene la base de datos configurada previamente?[y/n]"
 read ans
-if [ ans -eq "y"] then
+if [ ans -eq "n"] then
 	echo "Creando rol para la base de datos..."
 	sudo -u postgres createuser -PE -s sistema_ventas
 	echo "Creando base de datos..."
