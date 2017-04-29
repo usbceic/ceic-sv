b7b5df6c25efbfbdde7ad3b04887f4bcffb2ed29
Carlos Serrada <cserradag96@gmail.com>
2017-04-29 14:37:55 -0400

    Eliminado modelo antiguo en sql

--- models/Old/create_extensions.sql
+++
@@ -1,5 +0,0 @@
--- Base de Datos del Sistema de Ventas del CEIC
--- Hecho por Christian Oliveros
-
--- Extension para manejo de seriales unicos
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
--- models/Old/remove_extensions.sql
+++
@@ -1,5 +0,0 @@
--- Base de Datos del Sistema de Ventas del CEIC
--- Hecho por Christian Oliveros
-
--- Quitar extensiones si existian
-DROP EXTENSION IF EXISTS "uuid-ossp";
